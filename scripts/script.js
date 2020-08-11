9c189c631906d516d6cd4b318cde44684ca1a361
hearbreak <55318003+heartbreaks@users.noreply.github.com>
2020-08-11 13:33:20 +0500

    REFACTARED

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
-let photo = document.querySelector("img");
-let imgFloatHeight = parseFloat(window.getComputedStyle(photo).height);
-let imgFloatWidth = parseFloat(window.getComputedStyle(photo).width);
+let avatarObj = {
+  photo: document.querySelector("img"),
+  get height() {
+    return parseFloat(window.getComputedStyle(this.photo).height);
+  },
+  get width() {
+    return parseFloat(window.getComputedStyle(this.photo).width);
+  },
+};
 
 document.querySelector("img").addEventListener("mousemove", event => {
-  let y = -(((event.offsetY / imgFloatHeight) * 100 - 50) * 0.2);
-  let x = -(((event.offsetX / imgFloatWidth) * 100 - 50) * 0.2);
-  photo.style.boxShadow = `${x}px ${y}px 10px #6246ea`;
+  let y = -(((event.offsetY / avatarObj.height) * 100 - 50) * 0.2);
+  let x = -(((event.offsetX / avatarObj.width) * 100 - 50) * 0.2);
+  avatarObj.photo.style.boxShadow = `${x}px ${y}px 10px #6246ea`;
 });
