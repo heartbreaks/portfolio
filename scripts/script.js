a61b326f2f99a6705993b4887704057e6391b4e7
hearbreak <55318003+heartbreaks@users.noreply.github.com>
2020-08-11 13:12:16 +0500

    added the shadow at photo

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,9 @@
-let main = document.querySelector('main')
-let photo = document.querySelector('.photo')
-console.log(photo)
-// idk work it at notepad or not
-document.addEventListener('mousemove', (event) => {
-    let x = event.x
-    let y = event.y
-    // photo.style.backgroundColor = 'red'
-    photo.style.boxShadow = `${x} ${y} 15px black`
-    console.log(x, y)
-})
+let photo = document.querySelector("img");
+let imgFloatHeight = parseFloat(window.getComputedStyle(photo).height);
+let imgFloatWidth = parseFloat(window.getComputedStyle(photo).width);
+
+document.querySelector("img").addEventListener("mousemove", event => {
+  let y = -(((event.offsetY / imgFloatHeight) * 100 - 50) * 0.2);
+  let x = -(((event.offsetX / imgFloatWidth) * 100 - 50) * 0.2);
+  photo.style.boxShadow = `${x}px ${y}px 10px #6246ea`;
+});
